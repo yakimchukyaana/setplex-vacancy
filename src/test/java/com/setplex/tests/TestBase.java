3f03bf61a190411e509bc6e6f75613a642559a8d
yakimchukyaana <yakimchukyaana@gmail.com>
2023-09-21 19:52:39 +0500

    correct baseurl

--- src/test/java/com/setplex/tests/TestBase.java
+++ src/test/java/com/setplex/tests/TestBase.java
@@ -7,7 +7,7 @@ import com.codeborne.selenide.Configuration;
 public class TestBase {
     @BeforeAll
     static void beforeAll(){
-        Configuration.baseUrl = "https://setplex.com/";
+        Configuration.baseUrl = "https://setplex.com";
         Configuration.browserSize = "1920x1080";
         Configuration.pageLoadStrategy = "eager";
     }
