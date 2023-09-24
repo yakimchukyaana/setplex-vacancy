8c88e5e4801b9f828ab0e446cd088631d7153fa7
yakimchukyaana <yakimchukyaana@gmail.com>
2023-09-24 08:55:04 +0500

    add helpers and change testbase

--- src/test/java/com/setplex/tests/TestBase.java
+++ src/test/java/com/setplex/tests/TestBase.java
@@ -1,13 +1,45 @@
 package com.setplex.tests;
 
+import com.setplex.helpers.Attach;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import java.util.Map;
+import com.codeborne.selenide.logevents.SelenideLogger;
+import io.qameta.allure.selenide.AllureSelenide;
+import io.qameta.allure.Attachment;
+import static com.codeborne.selenide.Selenide.*;
 
 import com.codeborne.selenide.Configuration;
 
 public class TestBase {
     @BeforeAll
-    static void beforeAll(){
+    static void beforeAll() {
         Configuration.browserSize = "1920x1080";
         Configuration.pageLoadStrategy = "eager";
+        Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
+
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setCapability("selenoid:options", Map.<String, Object>of(
+                "enableVNC", true,
+                "enableVideo", true));
+
+        Configuration.browserCapabilities = capabilities;
+    }
+
+    @BeforeEach
+    void addListener() {
+        SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
+    }
+
+    @AfterEach
+    void addAttachments(){
+        Attach.screenshotAs("Screenshot");
+        Attach.pageSource();
+        Attach.browserConsoleLogs();
+        Attach.addVideo();
+
+        closeWebDriver();
     }
 }
